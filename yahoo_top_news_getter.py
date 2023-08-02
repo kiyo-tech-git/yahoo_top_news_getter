9a85d94a9fcece65e5b0094ea99fda24d6040063
猫とかまぼこ <67220994+kiyo-tech-git@users.noreply.github.com>
2023-08-02 12:23:56 +0900

    Add files via upload

---
+++ yahoo_top_news_getter.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+import re
+
+def main():
+    url = "https://news.yahoo.co.jp/" 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    contents = soup.find_all(href=re.compile('news.yahoo.co.jp/pickup'))
+    news_table = PrettyTable(
+        ["タイトル", "リンク"]
+    )
+    for content in contents:
+        title = content.text
+        link = content.attrs['href']
+        news_table.add_row([title, link])
+    print(news_table)
+
+if __name__=="__main__":
+    main()
